ea1be431c051d2e4f5c2383734fc9efdc85e5b95
SegfaultMaster <sashagoodguy@gmail.com>
2024-06-18 13:29:19 +0300

    msg

--- src/server.cpp
+++ src/server.cpp
@@ -148,8 +148,8 @@ int main(int argc, char **argv)
         std::cerr << "Failed to accept message";
         return 1;
     }
-
-    std::string message{strcmp("GET / HTTP/1.1", message_buffer.charPointer()) == 0? "HTTP/1.1 200 OK\r\n\r\n": "HTTP/1.1 404 Not Found\r\n\r\n"};
+    
+    std::string message{strcmp("GET / HTTP/1.1\r\n", message_buffer.charPointer()) == 0? "HTTP/1.1 200 OK\r\n\r\n": "HTTP/1.1 404 Not Found\r\n\r\n"};
     ssize_t bytes_send{send(client_fd, message.c_str(), message.length(), MSG_EOR)};
     
     if(bytes_send < 0)
