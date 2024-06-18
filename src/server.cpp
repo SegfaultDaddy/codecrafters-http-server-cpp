4ee639128eae673730f9d221acf1fc9bf3d8a558
SegfaultMaster <sashagoodguy@gmail.com>
2024-06-18 15:22:48 +0300

    msg

--- src/server.cpp
+++ src/server.cpp
@@ -101,7 +101,7 @@ int main(int argc, char **argv)
         }
     }
 
-    std::string message{"HTTP/1.1 200 OK\r\n\r\n"};
+    std::string message{};
     
     switch (found_sequence)
     {
@@ -112,7 +112,6 @@ int main(int argc, char **argv)
         {
             std::size_t response_start{message_buffer.find("echo/") + 5};
             std::string response{message_buffer.substr(response_start, message_buffer.find("HTTP") - 1 - response_start)};
-            std::cout << "Response: " << response << '\n';
             message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(response.length()) + "\r\n\r\n"
                 + response;
         }
