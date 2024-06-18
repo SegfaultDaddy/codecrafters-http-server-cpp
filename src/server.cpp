cb1d5d304ae8778661e297cad95987a1d37db1dd
SegfaultMaster <sashagoodguy@gmail.com>
2024-06-18 12:19:32 +0300

    msg

--- src/server.cpp
+++ src/server.cpp
@@ -12,9 +12,10 @@ int main(int argc, char **argv)
 {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
+    
     std::cout << "Logs from your program will appear here!\n";
     
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int server_fd{socket(AF_INET, SOCK_STREAM, 0)};
     
     if (server_fd < 0) 
     {
@@ -22,38 +23,41 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    int reuse = 1;
+    int reuse{1};
     
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) 
     {
         std::cerr << "setsockopt failed\n";
         return 1;
     }
-
+    
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(4221);
+    server_addr.sin_port = htons(4221); 
     
     if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) 
     {
         std::cerr << "Failed to bind to port 4221\n";
         return 1;
     }
-   
-    int connection_backlog = 5;
+    
+    int connection_backlog{5};
     
     if (listen(server_fd, connection_backlog) != 0) 
     {
         std::cerr << "listen failed\n";
         return 1;
     }
+    
     struct sockaddr_in client_addr;
-    int client_addr_len = sizeof(client_addr);
+    int client_addr_len{sizeof(client_addr)};
     std::cout << "Waiting for a client to connect...\n";
-    accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    int client_fd{accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len)};
+    std::string message{"HTTP/1.1 200 OK\r\n\r\n"};
+    ssize_t bytes_send{send(server_fd, message.c_str(), message.length(), MSG_OOB)};
     std::cout << "Client connected\n";
     close(server_fd);
-
+    close(client_fd);
   return 0;
 }
