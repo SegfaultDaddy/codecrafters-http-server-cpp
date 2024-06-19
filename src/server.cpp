2edb57576b46e560ff7cd3e239d0abc0182622a5
SegfaultMaster <sashagoodguy@gmail.com>
2024-06-19 21:30:28 +0300

    test

--- src/server.cpp
+++ src/server.cpp
@@ -177,6 +177,7 @@ std::string get_response_message(const std::string& request_message, const std::
         break;
     case 4:
         {
+            std::cout << "request_message: " << request_message << '\n';
             std::string text{find_string_in_between("\r\n\r\n", "\0", request_message)};
             std::cout << "Text: " << text << '\n';
             message = "HTTP/1.1 201 Created\r\n\r\n";
