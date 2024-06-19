16a417ab586a1a09bb38dac5d7ae7150b5da004a
SegfaultMaster <sashagoodguy@gmail.com>
2024-06-19 20:39:22 +0300

    test

--- src/server.cpp
+++ src/server.cpp
@@ -190,7 +190,7 @@ int send_server_response(int client_file_descriptor, int server_file_descriptor,
     }
 
     std::string response_message{get_response_message(request_message_buffer, directory_path)};
-    std::cout << "Response message: " << response_message << '\n';
+    std::cout << "Response message: " << client_file_descriptor << ' ' << response_message << '\n';
     ssize_t bytes_send{send(client_file_descriptor, response_message.c_str(), response_message.length(), MSG_EOR)};
 
     if(bytes_send < 0)
