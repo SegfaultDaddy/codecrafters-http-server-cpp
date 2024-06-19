29187954904ad407193d41288cc284334d0f4d30
SegfaultMaster <sashagoodguy@gmail.com>
2024-06-19 18:28:16 +0300

    test

--- src/server.cpp
+++ src/server.cpp
@@ -163,8 +163,9 @@ int send_server_response(int client_file_descriptor, int server_file_descriptor)
     }
 
     std::cout << "Accepted message: " << request_message_buffer << '\n';
-    std::string message{get_response_message(request_message_buffer)};
-    ssize_t bytes_send{send(client_file_descriptor, message.c_str(), message.length(), MSG_EOR)};
+    std::string response_message{get_response_message(request_message_buffer)};
+    std::cout << "Response message: " << response_message << '\n';
+    ssize_t bytes_send{send(client_file_descriptor, response_message.c_str(), response_message.length(), MSG_EOR)};
 
     if(bytes_send < 0)
     {
