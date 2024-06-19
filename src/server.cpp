86882f047d967be4a833e9a6ebd8ae0bc1c5753b
SegfaultMaster <sashagoodguy@gmail.com>
2024-06-19 20:56:48 +0300

    test

--- src/server.cpp
+++ src/server.cpp
@@ -148,6 +148,7 @@ std::optional<std::string> read_file(const std::string& filename, const std::str
 std::string get_response_message(const std::string& request_message, const std::string& directory_path)
 {
     std::string message{"HTTP/1.1 404 Not Found\r\n\r\n"};    
+    std::cout << "Here\n";
     switch (find_start_sequnce_index(request_message))
     {
     case 0:
@@ -192,7 +193,7 @@ int send_server_response(int client_file_descriptor, int server_file_descriptor,
     }
 
     std::string response_message{get_response_message(request_message_buffer, directory_path)};
-    std::cout << "HERE";
+ 
     ssize_t bytes_send{send(client_file_descriptor, response_message.c_str(), response_message.length(), MSG_EOR)};
 
     if(bytes_send < 0)
